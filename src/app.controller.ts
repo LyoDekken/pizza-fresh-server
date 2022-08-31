a8828ca2f24e82e3fc963ba538ee534768c12418
LyoDekken <humberto.araripe12@gmail.com>
2022-08-30 22:49:01 -0300

    feat: nest-js test railway

---
+++ src/app.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Req } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Request } from 'express';
+import { AppService } from './app.service';
+
+@ApiTags('status')
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  /**
+   * Recebe uma requisição GET e retorna um objeto de status
+   * da aplicação com a URL de documentação
+   * @param req Objeto de Request do Express
+   * @returns Objeto de status da aplicação
+   */
+  @Get()
+  @ApiOperation({
+    summary: 'Visualizar status da aplicação',
+  })
+  getAppStatus(@Req() req: Request) {
+    const baseUrl = req.protocol + '://' + req.get('host');
+    return this.appService.getAppStatus(baseUrl);
+  }
+}
---
+++ src/table/table.controller.ts
@@ -0,0 +1,67 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateTableDto } from './dto/create-table.dto';
+import { UpdateTableDto } from './dto/update-table.dto';
+import { Table } from './entities/table.entity';
+import { TableService } from './table.service';
+
+@ApiTags('table')
+@UseGuards(AuthGuard())
+@ApiBearerAuth()
+@Controller('table')
+export class TableController {
+  constructor(private readonly tableService: TableService) {}
+
+  @Get()
+  @ApiOperation({
+    summary: 'Listar todas as mesas',
+  })
+  findAll(): Promise<Table[]> {
+    return this.tableService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Visualizar uma mesa',
+  })
+  findOne(@Param('id') id: string): Promise<Table> {
+    return this.tableService.findOne(id);
+  }
+
+  @Post()
+  @ApiOperation({
+    summary: 'Criar uma mesa',
+  })
+  create(@Body() dto: CreateTableDto): Promise<Table> {
+    return this.tableService.create(dto);
+  }
+
+  @Patch(':id')
+  @ApiOperation({
+    summary: 'Editar uma mesa pelo ID',
+  })
+  update(@Param('id') id: string, @Body() dto: UpdateTableDto): Promise<Table> {
+    return this.tableService.update(id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({
+    summary: 'Remover uma mesa pelo ID',
+  })
+  delete(@Param('id') id: string) {
+    this.tableService.delete(id);
+  }
+}
